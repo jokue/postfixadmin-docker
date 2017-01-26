7b5cc94cb99cdaf2b175e7bab4b24e2599099a90
Johannes Kühnel <j.kuehnel@number13.de>
2017-01-26 17:50:30 +0100

    fixed sql command

--- init_db.sql
+++ init_db.sql
@@ -6,7 +6,7 @@ CREATE USER IF NOT EXISTS `postfix`@`%` IDENTIFIED BY '{{SQL_USR_PW}}';
 GRANT ALL PRIVILEGES ON vmail.* TO postfix;
 FLUSH PRIVILEGES;
 
-CREATE TABLE IF NOT EXISTS `vmail.x_tlspolicy` (
+CREATE TABLE IF NOT EXISTS `vmail`.`x_tlspolicy` (
     `id` int unsigned NOT NULL AUTO_INCREMENT,
     `domain` varchar(255) NOT NULL,
     `policy` enum('none', 'may', 'encrypt', 'dane', 'dane-only', 'fingerprint', 'verify', 'secure') NOT NULL,
